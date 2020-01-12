6bc57b9422de8fdd54df6463957092edaa8ecad8
Zhen <zhen@IT0011-Zhen.local>
2020-01-11 21:21:02 -0600

    completed proj0 - ReadPlanets

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -14,4 +14,26 @@ public class NBody {
    return radiusofU;
   }
 
+  public static Planet[] readPlanets(String filename1) {
+    In in = new In(filename1);
+    int N = in.readInt();
+    double radiusofU = in.readDouble();
+    Planet[] allPlanets = new Planet[N];
+
+    for (int i = 0; i < N; i++) {
+
+      allPlanets[i] = new Planet(in.readDouble(), in.readDouble(), in.readDouble(), in.readDouble(),in.readDouble(),in.readString());
+    }
+
+    return allPlanets;
+  }
+
+  //public static void main(String[] args) {
+
+//    double T = Double.parseDouble(args[0]);
+//    double dt = Double.parseDouble(args[1]);
+//    String filename = args[2];
+
+//  }
+
 }
