2319b6e8e81e6bc7cc94dacdb80405310057f6eb
Zhen <zhen@IT0011-Zhen.local>
2020-01-14 20:41:58 -0600

    completed proj0

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -81,6 +81,8 @@ public class NBody {
     StdOut.printf("%d\n", Planets.length);
     StdOut.printf("%.2e\n", radius);
     for (int i = 0; i < Planets.length; i++) {
+
+        Planets[i].imgFileName = Planets[i].imgFileName.replace("images/","");
         StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
                   Planets[i].xxPos, Planets[i].yyPos, Planets[i].xxVel,
                   Planets[i].yyVel, Planets[i].mass, Planets[i].imgFileName);
